0ede26692028e2fd1e81455424ad1e3c7ed1a171
Pietrzak Mariusz <web@pietrzakmariusz.dev>
2021-07-16 21:18:38 +0200

    turned off check PropTypes

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 function Rating({ value, text, color = '#f8e825' }) {
 	const classNameStars = [];
@@ -24,10 +24,10 @@ function Rating({ value, text, color = '#f8e825' }) {
 	);
 }
 
-Rating.propTypes = {
-	value: PropTypes.number.isRequired,
-	text: PropTypes.string.isRequired,
-	color: PropTypes.string,
-};
+// Rating.propTypes = {
+// 	value: PropTypes.number.isRequired,
+// 	text: PropTypes.string.isRequired,
+// 	color: PropTypes.string,
+// };
 
 export default Rating;
